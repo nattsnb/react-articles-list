3c48cb94ec30c34e8344fca08adba42cb1b44281
Natasza ASzatan <natiye.nb@gmai.com>
2024-10-02 19:20:26 +0200

    cleaned basic repo, added shared/api.js to hold fetches, created separate folders and files for different Routes, added BrowserRouter to main file.

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React, { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
