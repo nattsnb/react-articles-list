a26be5a0c1246df37cc317202cdd5ccb3c20e1c8
Natasza ASzatan <natiye.nb@gmai.com>
2024-10-06 20:49:58 +0200

    added component protected route that check if localStorage is set to admin, and allows to see task details only if true, else it navigates user to main page. added proteced route as elelemnt to route containing path to task details. added hook admin, and use effect function with dependancy on localStorage to set admin to admin or null

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,34 @@ import { Route, Routes } from "react-router-dom";
 import { TaskDetails } from "./taskDetails/index.jsx";
 import { TasksList } from "./tasksList/index.jsx";
 import styles from "./app.module.css";
+import { useEffect, useState } from "react";
+import { ProtectedRoute } from "./protectedRoute/protectedRoute.jsx";
 
 export function App() {
+  const isAdmin = localStorage.getItem("isAdmin");
+  const [admin, setAdmin] = useState(isAdmin);
+
+  useEffect(() => {
+    if (isAdmin === "admin") {
+      setAdmin("admin");
+    } else {
+      setAdmin(null);
+    }
+  }, [isAdmin]);
+
   return (
     <div>
       <h1>To Do List Example</h1>
       <Routes>
         <Route path="/" element={<TasksList />} />
-        <Route path="/task/:taskId" element={<TaskDetails />} />
+        <Route
+          path="/task/:taskId"
+          element={
+            <ProtectedRoute admin={admin}>
+              <TaskDetails />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
